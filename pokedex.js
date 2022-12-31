0b578d91de79beb76362033447191cdd325e04ec
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-31 18:14:27 +0100

    STYLE: added colours to the cards depending of the type

--- pokedex.js
+++ pokedex.js
@@ -6,7 +6,7 @@ async function getPokemons() {
     // const fetchResult = {};
 
     async function allPokemons() {
-        return fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150')
+        return fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
         .then(res => res.json());
     }
        
@@ -38,11 +38,34 @@ function renderPokemons(pokemon) {
         name$$.setAttribute("class","card-title");
 
     name$$.textContent = pokemon.id + " - " + pokemon.forms[0].name;
-    image$$.src = pokemon.sprites.other.dream_world.front_default;
+    image$$.src = pokemon.sprites.other["official-artwork"].front_default;
+
+    for (let i = 0; i < pokemon.types.length; i++){
+        if (pokemon.types[i].type.name === "grass"){card$$.classList.add("grass");}
+        if (pokemon.types[i].type.name === "fire"){card$$.classList.add("fire");}
+        if (pokemon.types[i].type.name === "water"){card$$.classList.add("water");}
+        if (pokemon.types[i].type.name === "bug"){card$$.classList.add("bug");}
+        if (pokemon.types[i].type.name === "normal"){card$$.classList.add("normal");}
+        if (pokemon.types[i].type.name === "poison"){card$$.classList.add("poison");}
+        if (pokemon.types[i].type.name === "electric"){card$$.classList.add("electric");}
+        if (pokemon.types[i].type.name === "ground"){card$$.classList.add("ground");}
+        if (pokemon.types[i].type.name === "fighting"){card$$.classList.add("fighting");}
+        if (pokemon.types[i].type.name === "fairy"){card$$.classList.add("fairy");}
+        if (pokemon.types[i].type.name === "psychic"){card$$.classList.add("psychic");}
+        if (pokemon.types[i].type.name === "rock"){card$$.classList.add("rock");}
+        if (pokemon.types[i].type.name === "ice"){card$$.classList.add("ice");}
+        if (pokemon.types[i].type.name === "dragon"){card$$.classList.add("dragon");}
+    }
 
     list$$.appendChild(card$$);
     card$$.appendChild(name$$);
     card$$.appendChild(image$$);
+
+    list$$.addEventListener("click", showStats(event));
+
+}
+
+function showStats (event){
 
 }
 
